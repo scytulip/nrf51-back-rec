3ca37a5f11b678285fbb64c4e284263aef4db9fb
Congyin Shi <scy.tulip@gmail.com>
2014-07-16 00:13:01 -0500

    Initialization

---
+++ arm/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ble_back_rec' 
+ * Target:  'nrf51822_xxaa_s110 (256K)' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
